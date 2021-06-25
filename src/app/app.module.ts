21907db1ab78a6c6820d82a42287e0ceaafb9c1b
soledad <fpsoto2@gmail.com>
2021-06-25 19:53:41 +0200

    añadido empleado a index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { primerComponente } from './components/primerComponente.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
     AppComponent, EmpleadosComponent,
-    primerComponente
+    primerComponente,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule
